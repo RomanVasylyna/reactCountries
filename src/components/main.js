aa6b8fd0ba03a4949a3efd270d2c0ee53dd6fe53
Roman <romanvasylyna888@gmail.com>
2021-04-28 19:38:32 +0300

    Main

--- src/components/main.js
+++ src/components/main.js
@@ -6,57 +6,44 @@ import Table from "react-bootstrap/Table";
 
 
 function Main() {
-//В useState указываем значение по-умолчанию, то есть пустой массив
-//первое значение это переменная а второе это функция которая будет обрабатывать ту переменную
-let [countries, setCountries] = useState([]);   
+let [countries, setCountries] = useState([]);
+let [filteredCountries, setFilteredCountries] = useState([]);
 
 const showName = name => alert(name);
 
-const filterCountries = e => {
-let val = e;
-// let newRegions = countries.filter(country => country.name.toLowerCase().includes(val) || country.capital.toLowerCase().includes(val));
-// console.log(newRegions);
-setCountries(countries.filter(country => country.name.toLowerCase().includes(val) || country.capital.toLowerCase().includes(val)));
+const filterCountries = val => {
+    setFilteredCountries(countries.filter(country => country.name.toLowerCase().includes(val) || country.capital.toLowerCase().includes(val)));
 };
 
-
-// useEffect - это хук который срабатывает при обновлении компонента
-// это эмуляция метода componentDidMount
-// срабатывает когда компонент полностью отрендерился
 useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
-    .then(data => {  
-    setCountries(data.map((country, index) => {
-    let {name, area, capital, population, alpha3Code} = country;
+    .then(data => {
+        setCountries(data.map((country, index) => {
+        let {name, area, capital, population, alpha3Code} = country;
 
-    return {
-    index:index+1,
-    name,
-    area,
-    capital,
-    population,
-    alpha3Code  
-    }
+        return {
+            index:index+1,
+            name,
+            area,
+            capital,
+            population,
+            alpha3Code
+        }
 
-    })); 
+        }));
     })
     .catch(err => console.log(err));
-}, []) //В параметре указываем условие для обновления компонента (если массив пустой)
+}, [])
 
 
 return(<>
-{/* Searching Countries */}
-<CountriesSearch filterCountries={ filterCountries }/>
-
-<Table striped hover bordered size="sm">
-{/* Table Header */}
-<CountriesHeader/>
-
-{/* Display All Countries */}
-<Countries countries={countries} showName={showName}/>
-
-</Table></>) 
+    <CountriesSearch filterCountries={ filterCountries }/>
+    <Table striped hover bordered size="sm">
+        <CountriesHeader/>
+        <Countries countries={filteredCountries.length ? filteredCountries : countries} showName={showName}/>
+    </Table>
+</>)
 
 
 }
